12b84186dacd5e3649b0c7036770526f19f77d34
Nitin-Sopan-Kshirsagar <44510597+Nitin-Sopan-Kshirsagar@users.noreply.github.com>
2018-10-27 01:57:59 +0530

    Logic1 comitting

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -1,1 +1,25 @@
-
+import java.util.Scanner
+class PrimeNumber
+{
+public static void main(String[] args)
+{
+Scanner sc=new Scanner(System.in);
+  System.out.println("Enter the number to be checked for being prime: ");
+  int number=sc.nextInt();
+  int count=0;
+  
+  for(int i=1;i<=number;i++)
+    if(number/i==0)
+    {
+    count++;
+    }
+  if(count==2)
+  {
+  System.out.println("Entered number is prime");
+  }
+  else
+  {
+  System.out.println("Entered nmber is not prime");
+  }
+}
+}
